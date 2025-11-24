a356d579a0ae4256d4b0f57a7c0cf993125fa3e8
Muwon Lee <lmw.hpc@gmail.com>
2025-11-24 10:04:19 +0900

    A_Way_Too_Long_Wrods.cpp

---
+++ A_Way_Too_Long_Words.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+string a;
+
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        if(a.length()<=10)
+            cout<<a<<endl;
+        else
+            cout<<a[0]<<a.length()-2<<a[a.length()-1]<<endl;
+    }
+}
